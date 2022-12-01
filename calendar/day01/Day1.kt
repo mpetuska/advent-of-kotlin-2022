73d473c24be63a6de46fc646e8af8d5d55668e47
Martynas Petuška <martynas@petuska.dev>
2022-12-01 12:02:47 +0000

    Day1

--- calendar/day01/Day1.kt
+++ calendar/day01/Day1.kt
@@ -5,10 +5,24 @@ import Lines
 
 class Day1 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        var localMax = 0
+        var globalMax = 0
+        for (line in input) {
+            if(line.isBlank()) {
+                localMax = 0
+            } else {
+                localMax += line.toInt()
+                if(localMax > globalMax) globalMax = localMax
+            }
+        }
+        return globalMax
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        val elves = mutableListOf(0)
+        input.forEach { line ->
+            if(line.isBlank()) elves += 0 else elves[elves.lastIndex] += line.toInt()
+        }
+        return elves.sorted().takeLast(3).sum()
     }
 }
