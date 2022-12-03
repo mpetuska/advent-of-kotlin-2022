cf40a41ee37c75e4d6353a78663531253b298fc8
Martynas Petuška <martynas@petuska.dev>
2022-12-03 14:22:51 +0000

    Day 3

--- calendar/day03/Day3.kt
+++ calendar/day03/Day3.kt
@@ -5,10 +5,16 @@ import Lines
 
 class Day3 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        return input
+            .map { it.chunked(it.length / 2) }
+            .map { (l, r) -> l.first(r::contains) }
+            .sumOf { if (it.isLowerCase()) it - 'a' + 1 else it - 'A' + 27 }
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        return input
+            .chunked(3)
+            .map { (a, b, c) -> a.first { it in b && it in c } }
+            .sumOf { if (it.isLowerCase()) it - 'a' + 1 else it - 'A' + 27 }
     }
 }
