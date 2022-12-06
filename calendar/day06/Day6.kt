0c60ce6d681943a3dadc2cc2ec03419b69f9e31a
Martynas Petuška <martynas@petuska.dev>
2022-12-06 09:03:19 +0000

    Day 6

--- calendar/day06/Day6.kt
+++ calendar/day06/Day6.kt
@@ -5,10 +5,14 @@ import Lines
 
 class Day6 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        return input.joinToString("")
+            .windowed(size = 4, step = 1, partialWindows = true)
+            .indexOfFirst { it.toSet().size == it.length } + 4
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        return input.joinToString("")
+            .windowed(size = 14, step = 1, partialWindows = true)
+            .indexOfFirst { it.toSet().size == it.length } + 14
     }
 }
